7e959278a5a4a9049dc1c8cd3d9d6f5c1e695d28
윤현민 <preweb1988@gmai.com>
2025-04-08 17:42:33 +0900

    2025.04.08 외장톰캣 구동용으로 어플리케이션 수정

--- denso_pda_frontEnd-main/src/main/java/com/denso/pdafrontend/PdaFrontendApplication.java
+++ denso_pda_frontEnd-main/src/main/java/com/denso/pdafrontend/PdaFrontendApplication.java
@@ -2,16 +2,24 @@ package com.denso.pdafrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients
 @ComponentScan(nameGenerator = PdaFrontendBeanNameGenerator.class)
 @SpringBootApplication
-public class PdaFrontendApplication {
+//public class PdaFrontendApplication {
+public class PdaFrontendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PdaFrontendApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PdaFrontendApplication.class);
 	}
 
 }
